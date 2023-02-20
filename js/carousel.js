d98c53404e951578c002fbfc98bb4b32c1e30d86
rrencat <104782737+rrencat@users.noreply.github.com>
2023-02-20 19:06:26 +0100

    carousel, kind of

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,7 @@
-const carouselContainer = document.querySelector(".blog-feature");
+const container = document.querySelector(".slideshow-container");
 
-const url = "https://www.rrencat.one/wp-json/wp/v2/posts?_embed";
+
+const url = "https://www.rrencat.one/wp-json/wp/v2/posts?_embed&per_page=100";
 
 
 async function getDetails() {
@@ -10,48 +11,34 @@ async function getDetails() {
         
         console.log(getPosts);
 
-        carouselContainer.innerHTML = "";
+        container.innerHTML = "";
 
-        
+        const blogPosts = getPosts;
 
-        for (let i = 0; i < getPosts.length; i++) {
-            const title = getPosts[i].title.rendered;
-            const image = getPosts[i]._embedded["wp:featuredmedia"][0].source_url;
-            const id = getPosts[i].id;
+        for (let i = 0; i < blogPosts.length; i++) {
+            const title = blogPosts[i].title.rendered;
+            const image = blogPosts[i]._embedded["wp:featuredmedia"][0].source_url;
+            const id = blogPosts[i].id;
 
-            // console.log(title);
-
+            console.log(title);
             
 
-            
-
-            if (i === 8) {
-                break;
-            }
-
-            carouselContainer.innerHTML += `<a href="details.html?id=${id}" class="card">
+            container.innerHTML += `<a href="details.html?id=${id}" class="card">
                                                 <div class="image" style="background-image: url(${image})"></div>
                                                 <div class="details">
                                                     <h4 class="name">${title}</h4>
                                                 </div>
-                                            </a>`;
-
-            
+                                            </a>`;  
         }
-
         
-
+        
         
     }
 
     catch(error) {
         console.log(error);
-        carouselContainer.innerHTML = message("error", error);
     }
     
 }
 
 getDetails();
-
-
-
